2fd6b4d8e0d81c3d9eef282e2bd08846d47c2b0a
linxing <linxing301@gmail.com>
2018-07-18 23:40:16 +0800

    complete useradmin

--- useradmin/infoadmin/models.py
+++ useradmin/infoadmin/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100) # port
+    password = models.CharField(max_length=100)
+    server = models.CharField(max_length=100)
+    crt_date = models.DateTimeField()
+    due_date = models.DateTimeField()
+    price = models.CharField(max_length=10)
+    tag = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.username
--- useradmin/infoadmin/views.py
+++ useradmin/infoadmin/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import User
+
+
+def index(request):
+    users = User.objects.all()
+    return render(request, 'index.html', {'users':users})
